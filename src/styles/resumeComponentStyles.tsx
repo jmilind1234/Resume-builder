5e6440e3d9dad52ab7c7b624f78aca06a0adb569
Milind Jain <jmilind1234@gmail.com>
2024-04-28 20:45:13 +0530

    added basic CSS for the profile header section.

---
+++ src/styles/resumeComponentStyles.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const ProfileHeader = styled.div`
+  border: 1px solid black;
+  width: 90%;
+  height: auto;
+  border-radius: 10px;
+  margin: auto;
+  position: relative;
+`;
+
+export const IntroDetails = styled.div`
+  // border: 1px solid black;
+  width: 50%;
+  height: auto;
+  border-radius: 10px;
+  margin-left: 3px;
+  margin-top: 3px;
+`;
+
+export const JobDetails = styled.div`
+  // border: 1px solid black;
+  width: 95%;
+  margin: auto;
+  border-radius: 10px;
+  display: flex;
+  margin-bottom: 10px;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-start;
+  height: auto;
+`;
